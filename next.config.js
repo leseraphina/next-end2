12791d5c44ba0e264639ad2709701ce2249c13f0
leseraphina <leseraphina@gmail.com>
2023-03-19 22:27:32 +0900

    4th

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  }
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
